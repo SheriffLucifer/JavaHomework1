ad13eea418bd45fc75aa68e84c6c3bc42ddfde4f
Oleg <olegashepelev@gmail.com>
2023-04-20 21:18:35 +0600

    Complete second task

---
+++ task2.java
@@ -0,0 +1,13 @@
+public class task2 {
+    public static void main(String[] args) {
+
+        int total = 1;
+        int i = 1;
+
+        for (i = 1; i <= 12; i++) {
+            total = total * i;
+        }
+        System.out.println(total);
+    }
+
+}
